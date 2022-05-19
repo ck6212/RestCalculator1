1f1ecad72ab541c3d5afe4f29121aaadc26f0719
ck6212 <Karapinar.can@hotmail.com>
2022-05-19 15:35:32 +0200

    initial commit

---
+++ src/main/java/com/example/restcalculator1/RestCalculator1Application.java
@@ -0,0 +1,13 @@
+package com.example.restcalculator1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestCalculator1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestCalculator1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restcalculator1/RestCalculator1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restcalculator1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestCalculator1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
